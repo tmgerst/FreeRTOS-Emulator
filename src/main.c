99f2185e3167a12de291a22bf2ad5819081cfc3a
Alex Hoffman <alxhoff@gmail.com>
2020-06-04 17:15:25 +0200

    Example code from youtube video

--- src/main.c
+++ src/main.c
@@ -24,74 +24,53 @@
 #define mainGENERIC_STACK_SIZE ((unsigned short)2560)
 
 static TaskHandle_t DemoTask = NULL;
+static aIO_handle_t slave_UDP_handle = NULL;
 
-typedef struct buttons_buffer {
-    unsigned char buttons[SDL_NUM_SCANCODES];
-    SemaphoreHandle_t lock;
-} buttons_buffer_t;
+//This is the SLAVE
 
-static buttons_buffer_t buttons = { 0 };
+#define MOSI_PORT 1234
+#define MISO_PORT 4321
 
-void xGetButtonInput(void)
+#define IPv4_addr "127.0.0.1"
+
+struct slave_args {
+    int prev_value;
+};
+
+void slaveRecv(size_t recv_size, char *buffer, void *args)
 {
-    if (xSemaphoreTake(buttons.lock, 0) == pdTRUE) {
-        xQueueReceive(buttonInputQueue, &buttons.buttons, 0);
-        xSemaphoreGive(buttons.lock);
-    }
-}
+    int recv_value = *((int *) buffer);
+    float sqrt_value = sqrtf(recv_value);
 
-#define KEYCODE(CHAR) SDL_SCANCODE_##CHAR
+    struct slave_args *my_slave_args = (struct slave_args *) args;
+
+    printf("Prev sent value was %d, new sqrt is %d\n", my_slave_args->prev_value, 
+            recv_value);
+
+    my_slave_args->prev_value = recv_value;
+
+    printf("Received %d, sqrt is %f\n", recv_value, sqrt_value);
+
+   if(aIOSocketPut(UDP, IPv4_addr, MISO_PORT, (char *)&sqrt_value, sizeof(sqrt_value)))
+       PRINT_ERROR("Failed to send sqrt to master");
+}
 
 void vDemoTask(void *pvParameters)
 {
-    // structure to store time retrieved from Linux kernel
-    static struct timespec the_time;
-    static char our_time_string[100];
-    static int our_time_strings_width = 0;
-
-    // Needed such that Gfx library knows which thread controlls drawing
-    // Only one thread can call tumDrawUpdateScreen while and thread can call
-    // the drawing functions to draw objects. This is a limitation of the SDL
-    // backend.
     tumDrawBindThread();
 
+    struct slave_args my_slave_args = {0};
+
+    slave_UDP_handle = aIOOpenUDPSocket(IPv4_addr, MOSI_PORT, sizeof(int), 
+            slaveRecv, (void *) &my_slave_args);
+
+    if(slave_UDP_handle == NULL){
+        PRINT_ERROR("Slave UDP socket failed to open");
+        exit(EXIT_FAILURE);
+    }
+
     while (1) {
-        tumEventFetchEvents(FETCH_EVENT_NONBLOCK); // Query events backend for new events, ie. button presses
-        xGetButtonInput(); // Update global input
-
-        // `buttons` is a global shared variable and as such needs to be
-        // guarded with a mutex, mutex must be obtained before accessing the
-        // resource and given back when you're finished. If the mutex is not
-        // given back then no other task can access the reseource.
-        if (xSemaphoreTake(buttons.lock, 0) == pdTRUE) {
-            if (buttons.buttons[KEYCODE(
-                                    Q)]) { // Equiv to SDL_SCANCODE_Q
-                exit(EXIT_SUCCESS);
-            }
-            xSemaphoreGive(buttons.lock);
-        }
-
-        tumDrawClear(White); // Clear screen
-
-        clock_gettime(CLOCK_REALTIME,
-                      &the_time); // Get kernel real time
-
-        // Format our string into our char array
-        sprintf(our_time_string,
-                "There has been %ld seconds since the Epoch. Press Q to quit",
-                (long int)the_time.tv_sec);
-
-        // Get the width of the string on the screen so we can center it
-        // Returns 0 if width was successfully obtained
-        if (!tumGetTextSize((char *)our_time_string,
-                            &our_time_strings_width, NULL))
-            tumDrawText(our_time_string,
-                        SCREEN_WIDTH / 2 -
-                        our_time_strings_width / 2,
-                        SCREEN_HEIGHT / 2 - DEFAULT_FONT_SIZE / 2,
-                        TUMBlue);
-
-        tumDrawUpdateScreen(); // Refresh the screen to draw string
+        tumEventFetchEvents(FETCH_EVENT_NONBLOCK);
 
         // Basic sleep of 1000 milliseconds
         vTaskDelay((TickType_t)1000);
@@ -119,12 +98,6 @@ int main(int argc, char *argv[])
         goto err_init_audio;
     }
 
-    buttons.lock = xSemaphoreCreateMutex(); // Locking mechanism
-    if (!buttons.lock) {
-        PRINT_ERROR("Failed to create buttons lock");
-        goto err_buttons_lock;
-    }
-
     if (xTaskCreate(vDemoTask, "DemoTask", mainGENERIC_STACK_SIZE * 2, NULL,
                     mainGENERIC_PRIORITY, &DemoTask) != pdPASS) {
         goto err_demotask;
@@ -135,8 +108,6 @@ int main(int argc, char *argv[])
     return EXIT_SUCCESS;
 
 err_demotask:
-    vSemaphoreDelete(buttons.lock);
-err_buttons_lock:
     tumSoundExit();
 err_init_audio:
     tumEventExit();
